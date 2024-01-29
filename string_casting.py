0609750264a1d0bc0cd7c78d220af5e04af4f6e0
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-29 15:54:28 +0530

    commiting string-casting file.

---
+++ string_casting.py
@@ -0,0 +1,10 @@
+num = '100'
+
+num2 = '200'
+
+print(num + num2)
+                    # two strings added will be concatenated.
+num3 = int(num)
+num4 = int(num2)
+                    # int () function is used to convert string to int
+print(num3 + num4)
