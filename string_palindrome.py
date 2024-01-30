a77be0fa368d83cbe2a05d48efe3e287d5abcae5
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-31 00:00:48 +0530

    commiting string_palindrome.py file.

---
+++ string_palindrome.py
@@ -0,0 +1,19 @@
+# string palindrome
+
+str = input('Enter the string : ')
+
+k = len(str)
+j = 1
+h = False
+for i in range(0, round(k/2)):
+    if(str[i]==str[-j]):
+        h = True
+        j+=1
+
+
+if(h==True):
+    print('The given string is a palindrome..')
+else:
+    print('The given string is not a palindrome..')
+
+
