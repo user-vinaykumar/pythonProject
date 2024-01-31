cc5209111890a4882225e9bc1fbca4c52f3de560
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-31 13:53:13 +0530

    starting list topic and edited the recent file.

--- lists.py
+++ lists.py
@@ -1,8 +1,9 @@
 courses = ['History', 'Math', 'CompSci', 'Biology', 'Chemistry']
-                # 0     1          2        3           4
+              # Index  # 0     1          2        3           4
 print(courses)
 
 print(courses[0])
 print(courses[3])
 
 print(courses[2])
+print(courses[len(courses)-1])  #accessing the last item (length - 1)
