925a717a6323a4b1bdc26d473e4e14721a2fdcea
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-31 14:06:10 +0530

    slicing with lists

---
+++ list_slicing.py
@@ -0,0 +1,15 @@
+courses = ['History', 'Math', 'Biology', 'Chemistry', 'CompSci']
+
+print(courses[0:3])
+
+print(courses[0:2])
+
+print(courses[0:])
+
+print(courses[:-1])
+
+print(courses[::-1])  #reverse the list
+
+print(courses[-1:-3])
+
+print(courses[-1:2])
