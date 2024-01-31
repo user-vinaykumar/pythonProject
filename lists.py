3ac406e12ac9f5b7ab3f1b731472da93c4008847
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-31 13:46:37 +0530

    starting list topic

---
+++ lists.py
@@ -0,0 +1,8 @@
+courses = ['History', 'Math', 'CompSci', 'Biology', 'Chemistry']
+                # 0     1          2        3           4
+print(courses)
+
+print(courses[0])
+print(courses[3])
+
+print(courses[2])
