4d06491f2300ea027b3a7214240b7fadb6518a3a
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-31 14:22:53 +0530

    appending the list

---
+++ list_append.py
@@ -0,0 +1,11 @@
+courses = ['History', 'Math', 'Chemistry', 'Art', 'Biology']
+
+courses.append('Environment')
+
+print(courses)
+
+courses2 = ['CompSci', 'Education']
+
+courses.append(courses2)
+
+print(courses)
