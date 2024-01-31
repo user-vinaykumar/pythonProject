3efe0d9aa7704781072ee4481a2c7d9061c37e37
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-31 15:55:11 +0530

    sorts list in reverse order

---
+++ list_sort_desc.py
@@ -0,0 +1,19 @@
+# here we are going to sort the list in reverse order, the original list is going to be
+# altered/overrided
+
+courses = ['Art', 'Chemistry', 'Polity', 'Biology', 'CompSci']
+
+courses.sort()  # sorts in asc / alphabetical order
+print(courses)
+
+
+courses.sort(reverse=True)
+print(courses)
+
+nums = [4,7,2,1,8,3]
+
+nums.sort() # sorts in asc / alphabetical order
+print(nums)
+
+nums.sort(reverse=True) # sorts in desc order
+print(nums)
