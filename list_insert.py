9a86b9d4163d36d765a229915f7458801e09bd8b
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-31 14:32:43 +0530

    inserting the list

---
+++ list_insert.py
@@ -0,0 +1,15 @@
+courses = ['History', 'Math', 'Biology', 'Chemistry', 'Art']
+
+courses.insert(0, 'Education') # Education is added at 0th index position
+
+print(courses)
+
+courses.insert(2, 'CompSci')  # CompSci is added at 2nd index position
+
+print(courses)
+
+courses2 = ['Socials', 'Polity']
+
+courses.insert(0, courses2)
+
+print(courses)
