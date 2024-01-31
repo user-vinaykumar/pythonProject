b2eb5963e9ce35a04bbfcb8a71916d80137a22bd
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-31 16:49:34 +0530

    list enumeration string start and edited the list enumerate in loop file.

---
+++ list_enumerate_string_start.py
@@ -0,0 +1,4 @@
+courses = ['Math', 'Polity', 'Socials', 'Chemistry', 'CompSci']
+
+for index, value in enumerate(courses, start=1):   # start=1 is printing starts at 1st index making 0 as 1st index.
+    print(index, value)                             # starting value of the index will be 1
