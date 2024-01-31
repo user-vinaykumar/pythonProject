94415c36b1b043e898a271e70f6d68db02899dba
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-31 17:15:33 +0530

    string to list convertion.

---
+++ string_to_list.py
@@ -0,0 +1,10 @@
+str = 'vinay, sharan, rajeshwari, shivalingaiah'
+
+list = str.split(', ')
+
+print(list)
+
+str2 = 'vinay--sharan--rajeshwari--shivalingaiah'
+
+list2 = str2.split('--')
+print(list2)
