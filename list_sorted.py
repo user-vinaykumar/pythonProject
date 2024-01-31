8ae82ca3dcbf23615a5859ee3c81215530ba38f7
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-31 15:46:09 +0530

    sorted function in list

---
+++ list_sorted.py
@@ -0,0 +1,11 @@
+courses = ['Art', 'Polity', 'Chemistry', 'Biology', 'Socials']
+
+sorted(courses)
+print(courses)  # just sort the list
+
+sorted_courses = sorted(courses)
+
+print(courses)  # original list
+
+print(sorted_courses)  # sorts the list in ascending order w/o altering the original list
+                        # the sorted list will be stored in new variable
