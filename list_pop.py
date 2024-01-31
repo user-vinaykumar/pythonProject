2393ff5ea43ce352084270671feb0d5982f6772d
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-31 14:58:41 +0530

    popping of the list

---
+++ list_pop.py
@@ -0,0 +1,7 @@
+courses = ['Art', 'Math', 'Biology', 'Chemistry', 'Socials']
+
+popped = courses.pop()  # removes the last item from the list and returns the value that we can store
+
+print(popped)
+print(courses)
+
