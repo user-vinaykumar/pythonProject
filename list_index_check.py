a665832abae1c6a0e3d348ae2dc48fb75ee218cb
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-31 16:24:47 +0530

    list index checking with True or False modified.

--- list_index_check.py
+++ list_index_check.py
@@ -3,3 +3,5 @@ courses = ['Polity', 'Art', 'Socials', 'Biology', 'Math']
 print('Math' in courses) # return True if the value ('Math') present in the list or else False
 
 print('Education' in courses)
+
+print('Polity' in courses[0])
