d363a7aaaf17380db3bd5813efa63b0e2347c786
user_vk_ <rossilorenzo81773@gmail.com>
2024-01-31 15:37:13 +0530

    sorting the list

---
+++ list_sort.py
@@ -0,0 +1,10 @@
+courses = ['Art', 'Chemistry', 'Biology', 'Education']
+
+courses.sort()
+print(courses)  # sorted in alphabetical order
+
+nums = [1,4,2,8,5,9,3]
+
+nums.sort()
+
+print(nums)  # sorts in ascending order
