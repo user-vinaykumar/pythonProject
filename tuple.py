ddad1c3e99772410419556e2500159e34a1dcd49
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-01 14:16:27 +0530

    tuples intro.

---
+++ tuple.py
@@ -0,0 +1,24 @@
+list1 = ['History', 'Math', 'Science', 'Socials', 'Chemistry']
+
+list2 = list1
+
+print(list1)
+print(list2)
+
+list1[0] = 'Art'
+
+print(list1)
+print(list2)
+
+tuple1 = ('History', 'Math', 'Socials', 'Science', 'Polity')
+tuple2 = tuple1
+
+print(tuple1)
+print(tuple2)
+
+ #tuple1[0]= 'Art'
+
+print(tuple1)
+print(tuple2)
+
+# unlike list tuple cannot be modified, tuple can only be accessed and read.
