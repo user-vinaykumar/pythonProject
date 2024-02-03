51349b99174e22f07e249aca8bacba7195ab2f3e
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-03 14:27:02 +0530

    list problem no.4 solved.

---
+++ list_problem4.py
@@ -0,0 +1,9 @@
+# program to merge two lists
+
+list1 = ['vinay', 'sharan', 'rajeshwari', 'shivalingaiah']
+
+list2 = ['mandya', 'mysuru', 'bengaluru', 'tumakuru']
+
+list1.extend(list2)
+
+print(list1)
