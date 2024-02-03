585fd9164bcea6595da31799b2fe95e9367e2330
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-03 16:33:27 +0530

    sets difference.

---
+++ sets_difference.py
@@ -0,0 +1,7 @@
+sets = {'History', 'Math', 'Chemistry', 'Physics', 'Biology'}
+
+places = {'History', 'Math', 'Bengaluru', 'Chamarajanagara', 'Tumakuru'}
+
+print(sets.difference(places))  # items that are in sets and not in places.
+
+print(places.difference(sets))  # items that are in sets2 and not in sets
