69d2116140c741482d4f848e66e79226dee5f779
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-03 16:19:48 +0530

    sets intersection

---
+++ sets_intersection.py
@@ -0,0 +1,13 @@
+sets = {'vinay', 'sharan', 'rajeshwari', 'shivalingaiah'}
+sets2 = {'rajeshwari', 'vinay', 'mandya', 'mysuru'}
+
+print('rajeshwari' in sets)
+
+print(sets.intersection(sets2))  # items that are in sets and also in sets2
+
+#print(sets.difference(sets2))    # items that are in sets and not in sets2
+
+#print(sets2.difference(sets))   # items that are in sets2 and not in sets
+
+
+
