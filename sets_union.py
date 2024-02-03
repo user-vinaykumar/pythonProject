8895c826ba6ab226211603aab2b94dfe8c0a77e5
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-03 16:37:21 +0530

    sets union.

---
+++ sets_union.py
@@ -0,0 +1,5 @@
+sets = {'History', 'Math', 'Socials', 'Polity', 'Chemistry'}
+
+places = {'Mandya', 'Mysuru', 'Bengaluru'}
+
+print(sets.union(places))      # adds both the sets and present those in single set.
