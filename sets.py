a60620181463cad20018cd62d588e503f09416d9
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-03 15:53:02 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,6 @@
+sets = {'History', 'Math', 'Chemistry', 'Biology', 'Socials'}
+
+print(sets)  # unordered items with no duplicate values.
+
+ # every time we run the program the output will be in different order all the time.
+
