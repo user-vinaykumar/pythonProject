06ad224ab608098a798382eb4cfecc59a4099671
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-03 14:55:43 +0530

    list problem no.5 commented.

---
+++ list_problem5.py
@@ -0,0 +1,14 @@
+'''list = ['vinay', 'sharan', 'rajeshwari', 'shivalingaiah']
+
+k = len(list)
+j =1
+list.insert(0, list[k-j])
+list.append(list[j])
+print(list)
+list.remove(list[j])
+list.remove(list[k-1])
+
+print(list)
+
+
+'''
