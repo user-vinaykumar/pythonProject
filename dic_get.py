dfe0aaa2c10d16ad5b755f9a5ff581b3198ef58e
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-04 15:27:18 +0530

    dictionary and .get() methods.

---
+++ dic_get.py
@@ -0,0 +1,10 @@
+student = {'name': 'vinay', 'age':26, 'subject':'CompSci'}
+
+print(student.get('name'))
+
+print(student.get('age'))
+
+print(student.get('phone')) # passing the wrong key in .get() will return None
+
+print(student['phone'])  # passing the wrong key w/o .get() will return KeyError
+
