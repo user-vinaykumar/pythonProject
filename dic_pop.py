e0445a20e171bb65aa57b9fb8c2dffa67651997d
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-04 16:48:35 +0530

    dic pop().

---
+++ dic_pop.py
@@ -0,0 +1,8 @@
+student = {'name':'vinay', 'place':'mandya', 'age':27, 'subject':'Math'}
+
+student.pop('age')
+
+print(student)
+
+place = student.pop('place')  # we can store the popped item in another variable.
+print(place)
