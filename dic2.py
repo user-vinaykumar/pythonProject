477b4c7dcab8270ed824bdd896b2eaa995f165d4
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-04 16:55:50 +0530

    dic

---
+++ dic2.py
@@ -0,0 +1,9 @@
+student = {'name':'vinay', 'place':'mandya', 'age':27, 'subject':'Math'}
+
+print(len(student)) # prints the length of the student dic
+
+print(student.keys())  # prints the keys of the student dic
+
+print(student.items())  # prints the items with its keys of the student dic, helps in looping the dic.
+
+
