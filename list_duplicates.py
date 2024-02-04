82effe16838f093806d34e3e67a613e04a5cea01
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-04 14:57:35 +0530

    duplicates removed from the list.

---
+++ list_duplicates.py
@@ -0,0 +1,7 @@
+# remove duplicate values from the list
+
+list2 = [1,4,6,2,8,9,3,4,5,8,9,2,1,0,8,3,2,7,8,1,8,9,2,3,6,12,34,56,54,32,34,12,12]
+
+duplicates_removed = list(set(list2))
+
+print(duplicates_removed)
