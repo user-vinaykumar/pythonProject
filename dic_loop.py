89da48d895aa30a8fffd6b871567cc7f1cae211a
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-04 16:59:15 +0530

    dic loop

---
+++ dic_loop.py
@@ -0,0 +1,9 @@
+student = {'name':'vinay', 'place':'mandya', 'age':27, 'subject':'Math'}
+
+for keys in student:
+    print(keys)
+
+print('---------')
+
+for keys, values in student.items():
+    print(keys, values)
