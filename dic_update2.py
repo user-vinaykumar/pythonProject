0f80e5868a8f310833c986352f089b92b192084a
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-04 16:44:17 +0530

    dic update

---
+++ dic_update2.py
@@ -0,0 +1,7 @@
+# updating the multiple values in 1 go
+
+student = {'name':'vinay', 'place':'mandya', 'subject':'CompSci'}
+
+student.update({'place':'mysuru', 'subject':'Math'}) # flower braces inside bracket is necessary to update all the values in 1 go.
+
+print(student)
