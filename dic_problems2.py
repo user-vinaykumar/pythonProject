3dfeecbf2da201c25592ebed507b01e0a6257477
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-05 14:55:57 +0530

    converting two lists into a dictionary.

---
+++ dic_problems2.py
@@ -0,0 +1,9 @@
+# convert two lists into a dictionary using built in methods
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+dic = dict(zip(keys, values))
+
+print(dic)
+
---
+++ dic_problems3.py
@@ -0,0 +1,14 @@
+# convert two lists into a dictionary using update method
+
+keys = ['vinay', 'sharan', 'rajeshwari', 'shivalingaiah']
+
+value = [0,1,2,3]
+j = len(keys)
+
+dic2 = {}
+
+for i in range(j):
+    dic2.update({value[i]:keys[i]})
+
+
+print(dic2)
