ed16c62e80605dcd3e450966c7b045ee809022ac
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-05 14:58:44 +0530

    removed unwanted lines.

--- dic2.py
+++ dic2.py
@@ -4,6 +4,6 @@ print(len(student)) # prints the length of the student dic
 
 print(student.keys())  # prints the keys of the student dic
 
-print(student.items())  # prints the items with its keys of the student dic, helps in looping the dic.
+print(student.items())  # prints the items with its keys, helps in looping the dic.
 
 
