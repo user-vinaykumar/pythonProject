3e1838c5a57f13b217d9e4c5e733f27c88e412be
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-06 18:44:25 +0530

    is and equal comparison.

---
+++ is_and_equal.py
@@ -0,0 +1,10 @@
+a = [1,2,3]
+b = [1,2,3]
+
+print(a==b)   # equates the two lists
+
+print(a is b)  # does not belong to same memory location. both are different objects.
+
+print(id(a))
+print(id(b))
+
