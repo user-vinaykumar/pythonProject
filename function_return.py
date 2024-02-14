0b0ac18398b441101b38318a1a84d7b2f678f3fb
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-14 17:09:41 +0530

    function return in python.

---
+++ function_return.py
@@ -0,0 +1,15 @@
+def hello_world():
+    return 'Hello World!'
+
+hello_world()  # returns empty bcz we are just returning the value and not printing the value.
+
+print(hello_world())
+
+# the above example is just like len() function only with print(len('abc'))
+# it will print the value otherwise with just len('abc') it will just return the value
+
+def number_game():
+    return 25
+
+number_game()
+print(number_game())
