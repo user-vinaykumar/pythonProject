37fc526ac9f98f2123b19667e257309113fb49d1
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-14 17:10:23 +0530

    function return in python.

--- function_return.py
+++ function_return.py
@@ -4,6 +4,7 @@ def hello_world():
 hello_world()  # returns empty bcz we are just returning the value and not printing the value.
 
 print(hello_world())
+print(hello_world().upper())
 
 # the above example is just like len() function only with print(len('abc'))
 # it will print the value otherwise with just len('abc') it will just return the value
