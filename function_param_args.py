d3ea7ba312518501b4a1548d975320072687a5de
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-14 17:30:16 +0530

    function parameters and arguments in python.

---
+++ function_param_args.py
@@ -0,0 +1,19 @@
+def function(greeting):
+    return '{} Guys!'.format(greeting)
+
+print(function('Hi'))
+
+def home(name, house):
+    return "This is {}'s {}".format(name, house)
+
+print(home('vinay', 'home'))
+
+def fun(name, house):
+    print(f"this is {name}'s {house}")
+
+fun('sharan', 'home')
+
+def abc(name, work):
+    print("this is {}'s {}".format(name, work))
+
+abc('vinay', 'office')
