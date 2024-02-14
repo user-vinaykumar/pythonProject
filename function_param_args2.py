ee780f9c3dac42769e11a508f7e6ecb59a6daede
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-14 17:38:21 +0530

    function arguments and its order in python.

---
+++ function_param_args2.py
@@ -0,0 +1,8 @@
+def fun1(name, work):
+    print("this is {}'s {}".format(name, work))
+
+fun1('vinay', work = 'office')
+
+#fun1(work='vinay', 'sharan') # position argument after keyword argument.
+# arguments should be followed in the same order.
+
