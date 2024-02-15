7a5643a76bf777b908db49f292a2a114d12d6bea
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-15 18:24:39 +0530

    charecter count in a given string.

---
+++ string_problems.py
@@ -0,0 +1,8 @@
+# find the number of charecters in a given string
+
+string = input('Enter the string : ')
+j = 0
+for i in string:
+    j+=1
+
+print(f'The number of charecter in a {string} : ', j)
