7ab932bdf984d14f61c68f06c7c76fab7bc70b75
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-15 15:23:27 +0530

    math.floor in Python.

---
+++ function_math.floor.py
@@ -0,0 +1,12 @@
+import math
+
+
+def floor(number):
+    print(math.floor(number))
+
+
+floor(4.5)  # if the number is decimal like 4.5 it will return 4, likewise 3 for 3.decimal and
+# for number for any number.decimal
+# ceil gives the largest integer of the decimal
+# floor gives the lowest integer of that decimal
+floor(7)
