7de19374ccda8c9648622647b40037bc3ba906a2
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-15 14:01:03 +0530

    function without definition or body.

---
+++ function_no_definition.py
@@ -0,0 +1,8 @@
+# here we are going to see the function with no definition and how it is going to execute
+# if we not pass anything in its body.
+
+def function():
+    pass
+
+print(function())  # will give None as the output.
+
