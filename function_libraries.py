38c8d6ae437f73c4aefb8f53779807f32fcecbc2
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-15 14:23:46 +0530

    function libraries of python.

---
+++ function_libraries.py
@@ -0,0 +1,8 @@
+# here we are going to see the python library functions.
+
+import math  # importing math library into this file.
+
+def square_root_of_num(num):
+    return int(math.sqrt(num))       # from math library we are using sqrt() function.
+
+print(square_root_of_num(4))
