3633628ee331ac5fd45d259197b3847a3275392c
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-15 16:07:47 +0530

    formatted the lines in a file.

--- function_math.fmod.py
+++ function_math.fmod.py
@@ -3,22 +3,23 @@
 import math
 
 
-
 def fmod(number, j):
     return math.fmod(number, j)
 
-month_number = {'January' : 1,
-                'February' : 2,
-                'March' : 3,
-                'April' : 4,
-                'May' : 5,
-                'June' : 6,
-                'July' : 7,
-                'August' : 8,
-                'September' : 9,
-                'October' : 10,
-                'November' : 11,
-                'December' : 12}
+
+month_number = {'January': 1,
+                'February': 2,
+                'March': 3,
+                'April': 4,
+                'May': 5,
+                'June': 6,
+                'July': 7,
+                'August': 8,
+                'September': 9,
+                'October': 10,
+                'November': 11,
+                'December': 12}
+
 
 def isMonth(month_num):
     if fmod(month_number[month_num], 2) == 0:
@@ -26,9 +27,6 @@ def isMonth(month_num):
     else:
         return 'The month is Odd Month!'
 
+
 print(isMonth('January'))
 print(isMonth('February'))
-
-
-
-
