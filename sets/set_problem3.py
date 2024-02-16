1022822401b3ddfb08a3e48c40267345b4695c4e
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-16 18:28:08 +0530

    calculating the length of set.

---
+++ sets/set_problem3.py
@@ -0,0 +1,11 @@
+# calculate the length of the following set.
+
+sets = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'}
+
+
+def setLength(item):
+    k = len(item)
+    print(k)
+
+
+setLength(sets)
