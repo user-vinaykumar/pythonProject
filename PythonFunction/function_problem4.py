5d87dd8b17626d0e104f5ac2e022d862df9f815c
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-16 16:41:38 +0530

    program to remove the duplicate items from the list and storing output in a set.

---
+++ PythonFunction/function_problem4.py
@@ -0,0 +1,9 @@
+# write a program to take a list as an input and remove the duplicates and output
+# should be in set.
+
+def duplicateEliminator(item):
+    noDuplicates = set(item)
+    print(noDuplicates)
+
+
+duplicateEliminator([1, 2, 3, 3, 4, 5])
