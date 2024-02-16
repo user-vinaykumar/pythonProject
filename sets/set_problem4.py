f6100cf3fb238f157da9e6d2fae25278105673cb
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-16 18:36:03 +0530

    discard an item from the set.

--- sets/set_problem4.py
+++ sets/set_problem4.py
@@ -1,7 +1,13 @@
 # remove an element from a set.
 
 sets = {'a', 'vinay', 4, 'sharan'}
+sets2 = tuple(sets)
 
 sets.remove('a')  # remove will remove particular item, and pop will remove the last item.
 
 print(sets)
+
+sets3 = set(sets2)
+
+sets3.discard('sharan')
+print(sets3)
