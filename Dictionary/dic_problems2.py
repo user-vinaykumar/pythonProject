82d7f7623509823f02a18cb46f6de0562c157f7b
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-17 13:23:59 +0530

    dictionaty problems2 in python.

---
+++ Dictionary/dic_problems2.py
@@ -0,0 +1,8 @@
+# return the value of a particular key. key = 'India'
+
+namesOfCountry = {'China' : 'Beijing', 'India': 'Bengaluru', 'Mexico': 'Mexico City'}
+
+def returnfun(item):
+    return item['India']
+
+print(returnfun(namesOfCountry))
