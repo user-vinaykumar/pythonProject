7d16fc1d8901778ae4781ae2f1f0b4ef701b9445
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-18 16:50:07 +0530

    line formatted for the previous programme.

--- Problems/FindDuplicates.py
+++ Problems/FindDuplicates.py
@@ -1,21 +1,22 @@
 # find the duplicate elements that are repeated more than once in a list.
+# this programme is not dynamic only applicable to some problems and not all of them.
 
 def findDupicates(nums):
-    tortoise = nums[0]
-    hare = nums[0]
+    tortoise = nums[0]  # 1
+    hare = nums[0]  # 1
     while True:
-        tortoise = nums[tortoise]
-        hare = nums[nums[hare]]
+        tortoise = nums[tortoise]  # 4
+        hare = nums[nums[hare]]  # 4
         if tortoise == hare:
             break
 
-    ptr1 = nums[0]
-    ptr2 = tortoise
+    ptr1 = nums[0]  # 1
+    ptr2 = tortoise  # 4
     while ptr1 != ptr2:
-        ptr1 = nums[ptr1]
-        ptr2 = nums[ptr2]
+        ptr1 = nums[ptr1]  # 4
+        ptr2 = nums[ptr2]  # 4
 
     return ptr1
 
 
-print(findDupicates([1, 2, 3, 4, 4, 5, 2, 4, 9, 2, 1, 6, 7]))
+print(findDupicates([10, 11, 13, 13, 14, 13, 15]))
