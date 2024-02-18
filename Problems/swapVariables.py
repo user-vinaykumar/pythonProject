e2f6f1e5adec14babb2ffc06af1c6d5d63256854
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-18 17:35:07 +0530

    swap two variable in python.

---
+++ Problems/swapVariables.py
@@ -0,0 +1,11 @@
+# swap two variables in python.
+
+a, b = 3, 5
+print('a value is ', a)
+print('b value is ', b)
+b, a = a, b
+
+print('-----------------------------')
+
+print('a value is ', a)
+print('b value is ', b)
