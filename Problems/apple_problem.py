e9d1d16873417da490652472ca61eed0ec2ce6b1
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-19 16:07:06 +0530

    apple problem unsolved.

---
+++ Problems/apple_problem.py
@@ -0,0 +1,14 @@
+'''# given an array of integers return true if the number of occurences of each value in the array
+# is unique or false otherwise.
+
+# integer = [1,1,2,2,2,3]
+# output = True
+# 1 has occured twice, 2 has occured thrice and 3 has occurred once. hence true.
+import counter
+def occurr(item):
+    count = counter(item)
+    print(count)
+    #for i in item:
+
+occurr([1,2])
+'''
