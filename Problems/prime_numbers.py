fcd4c635ddd8b0db19314d3e5edcd209240b6aeb
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-19 14:26:26 +0530

    problem in python - Prime numbers.

--- Problems/prime_numbers.py
+++ Problems/prime_numbers.py
@@ -3,3 +3,10 @@
 prime = [i for i in range(2, 100) if 0 not in [i%n for n in range(2, i)]]
 
 print(prime)
+
+
+def isPrime(item):
+    primeList = [i for i in item if 0 not in [i%n for n in range(2, i)]]
+    print(primeList)
+
+isPrime([2,777,33,41,29,22,999,33])
