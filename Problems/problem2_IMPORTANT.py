2168e572bf23d42fa2ff1b9b25264089a641ba2c
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-20 15:14:27 +0530

    merge two dictionaries and the value if present in both dictionaries.

--- Problems/problem2_IMPORTANT.py
+++ Problems/problem2_IMPORTANT.py
@@ -1,11 +1,12 @@
 # merge two dictionaries if same key value pair appears then the value must be added.
 
-inv = {'sword' : 1,
-       'potion' : 3}
+inv = {'sword': 1,
+       'potion': 3}
 
-loot = {'sword' : 1,
-        'potion' : 2,
-        'shield' : 1}
+loot = {'sword': 1,
+        'potion': 2,
+        'shield': 1}
+
 
 # need to merge these two dictionaries
 
@@ -14,9 +15,10 @@ def mergeTwoDictionaries(item, wood):
     set1 = set(item)
     set2 = set(wood)
     set3 = set1.union(set2)
-    newlist = {k : item.get(k, 0) + wood.get(k, 0) for k in set3}
+    newlist = {k: item.get(k, 0) + wood.get(k, 0) for k in set3}
     return newlist
+
 
 print(mergeTwoDictionaries(inv, loot))
 
-#new = {k : inv.get(k, 0)} + loot.get(k, 0) for k in set(inv )
+# new = {k : inv.get(k, 0)} + loot.get(k, 0) for k in set(inv )
