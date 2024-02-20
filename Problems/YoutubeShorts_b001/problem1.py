c6d470d236d25089f0aeff98fc9b6b716bfd7698
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-20 15:57:44 +0530

    move second element of a list to the last index.

--- Problems/YoutubeShorts_b001/problem1.py
+++ Problems/YoutubeShorts_b001/problem1.py
@@ -2,11 +2,22 @@
 
 inv = ['gem', 'sword', 'shield', 'potion']
 
+def funct(item):
+    length = len(inv)
+    indx = inv.index(item)
+
+    inv.insert(length-1, inv.pop(indx))
+    return inv
+
+print(funct('sword'))
+
+print('ffffffffffffffffffffffffffffff')
 
 # move 'gem' to the end of the list
 
 def move(item):
-    item.append(item.pop(0))
+    indx = inv.index('gem')
+    inv.append(inv.pop(indx))
     return item
 
 
