ca0aa1b3fc39a2fca15a326dbed1b65e92b49a07
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-21 14:56:26 +0530

    importing everything from the module.

---
+++ Module/fourth_python.py
@@ -0,0 +1,10 @@
+# importing everything from the module to another module.
+
+from Python_module import *
+
+names = ['vinay', 'sharan', 'rajeshwari', 'shivalingaiah', 'mandya']
+
+indx = indexfinder(names, 'mandya')
+print(f'the index of the value mandya is : {indx}')
+print('--------------')
+print(variable)
