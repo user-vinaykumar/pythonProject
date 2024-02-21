464360d1a0886f047e155abfe31bad153c2d5408
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-21 14:33:17 +0530

    python module built for next file to utilize.

---
+++ Module/firstPython.py
@@ -0,0 +1,7 @@
+import Python_module as pm
+
+courses = ['History', 'Math', 'Physics', 'Chemistry', 'Socials']
+
+ind = pm.indexfinder(courses, 'Chemistry')
+
+print(f'the index of the value is : {ind}')
