b302dd500ce1a9cae5fc272a24c574900ca6dab1
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-21 15:42:09 +0530

    importing random standard library.

---
+++ Module/standard_library.py
@@ -0,0 +1,8 @@
+# importing modules from the standard library.
+
+import random
+
+names = ['vinay', 'sharan', 'swaroop', 'rakshith', 'jayaprakasha', 'jagadeesha']
+
+random_names = random.choice(names)
+print(random_names)
