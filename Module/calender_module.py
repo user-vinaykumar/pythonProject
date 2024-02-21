3ea0d04f7b96ea162fbd9ac774645b2c70478eb0
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-21 16:06:08 +0530

    importing calender to check whether the given year is leap or not.

---
+++ Module/calender_module.py
@@ -0,0 +1,14 @@
+# importing calender library to calculate whether a given year is leap or not.
+
+import calendar
+
+
+def leapCheck(item):
+    year = calendar.isleap(item)
+    if year:
+        print(f'the year {item} is a leap Year.')
+    else:
+        print(f'the year {item} is not a leap year.')
+
+
+leapCheck(2018)
