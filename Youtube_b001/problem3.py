5ec9685e0dba0c40658efdc3333be4ac3f50e1a9
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-21 12:23:42 +0530

    could not solve the problem.

---
+++ Youtube_b001/problem3.py
@@ -0,0 +1,23 @@
+"""profile = {'name' : 'n/a',
+           'phone' : 'n/a',
+           'email' : 'n/a'}
+
+user_input = {'vinay' : 78990,
+              'sharan' : 733852}
+
+
+
+def entertheprofilevalues(item, wood):
+    s = set(wood)
+    print(s)
+    wood2 = {k : j for j, k in wood.items()}
+    s2 = set(wood2)
+    print(s2)
+    for i in item:
+        profile[i] =
+
+
+
+
+print(entertheprofilevalues(profile, user_input))
+"""
