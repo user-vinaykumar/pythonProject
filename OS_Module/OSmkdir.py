416c461dc3a181d0f99cc0df8776898181eb2ffe
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-22 14:29:57 +0530

    mkdir('item') creates a file / folder item in the current directory.

--- OS_Module/OSmkdir.py
+++ OS_Module/OSmkdir.py
@@ -2,7 +2,11 @@
 
 import os
 
+
 def createdir(item):
     os.mkdir(item)
 
+
 print(createdir('this.py'))  # creates 'this' file / folder in OS_Module folder.
+
+# only one file or folder can be created.
