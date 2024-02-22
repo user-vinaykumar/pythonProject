731a401b95edf45dda64930fb55cf39f56c04d64
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-22 12:41:27 +0530

    function created for a normal execution.

--- OS_Module/__init__.py
+++ OS_Module/__init__.py
@@ -8,3 +8,20 @@ indx = indexfinder(dir(os), 'getcwd')  # imported the Python module that helps i
 
 print(indx)
 print(len(dir(os)))
+
+from itertools import groupby
+
+# first method to do the following thing without importing any custom module.
+
+# directoryGroup = groupby(dir(os), key=lambda n : n[0])
+#
+# for ke, val in directoryGroup:
+#     print(ke, list(val))
+
+# by importing the custom module created in previous problem.
+
+from Problems import groupByFirstchar
+
+groupByFirstchar.groupby_firstCharecter(dir(os))
+
+print(help('kill'))
