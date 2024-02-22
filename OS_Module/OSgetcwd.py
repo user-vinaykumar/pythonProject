5ae74aa73e90cd10a730db143b070e53958e3aa5
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-22 13:45:16 +0530

    current working directory by os module function.

---
+++ OS_Module/OSgetcwd.py
@@ -0,0 +1,7 @@
+# prints the current working directory. ---> current folder we are working in.
+import os
+
+def currentWorkingDirectory():
+    print(os.getcwd())   # getcwd() ---> function that gets our output present in os module.
+currentWorkingDirectory()
+
