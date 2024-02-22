dfd546b680c1a1b536701cd31feb590c574130cc
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-22 13:31:16 +0530

    traffic light module edited to make use of the module for another file.

--- Youtube_b001/trafficLight.py
+++ Youtube_b001/trafficLight.py
@@ -7,18 +7,22 @@ light = [('Green', 2), ('Red', 2), ('Yellow', 1)]
 
 
 #count = 1
-while True:
+
+def traffic_light_simulator():
     i = 0
-    length = len(light)
-    c, t = light[i]
-    print(c)
-    time.sleep(t)
-    if i == length-1:
-        i = 0
-    else:
-        i+=1
+    while True:
+        length = len(light)
+        c, t = light[i]
+        print(c)
+        time.sleep(t)
+        if i == length-1:
+            i = 0
+        else:
+            i+=1
     #if count == 6:
      #   break
 
    # else:
     #    count+=1
+
+traffic_light_simulator()
