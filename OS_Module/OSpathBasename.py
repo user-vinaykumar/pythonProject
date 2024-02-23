1eeda757c3076e8e539e9b3281696459bf865858
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-23 16:34:16 +0530

    printing base file name of the path.

---
+++ OS_Module/OSpathBasename.py
@@ -0,0 +1,7 @@
+# prints the base file name in the path we provide in a function.
+
+import os
+
+print(os.path.basename('C:/Users/ADMIN/Documents/Sound recordings/Recording.m4a'))
+# prints Recording.m4a -- the base file present in the entire path
+
