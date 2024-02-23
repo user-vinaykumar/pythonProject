dbbf0190f63ec51c335384376120171d0d2f2431
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-23 16:49:51 +0530

    file modified. For rough work.

--- OS_Module/OSpathsplit.py
+++ OS_Module/OSpathsplit.py
@@ -1,6 +1,16 @@
 # splits the base directory name and the base file name.
 
 
+
 import os
 
 print(os.path.split("C:/Users/ADMIN/Documents/Sound recordings/Recording.m4a"))
+
+
+
+# rough work
+
+from Problems import groupByFirstchar
+
+groupByFirstchar.groupby_firstCharecter(dir(os.path))
+
