6ab0e57bfa80e32ed35d8620762387df654215cd
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-23 16:39:46 +0530

    prints the directory name / path -- of the file we provide inside a function.

---
+++ OS_Module/OSpathdirectoryname.py
@@ -0,0 +1,7 @@
+# prints the directory name of the file we present in provided in the ()
+
+import os
+
+print(os.path.dirname("C:/Users/ADMIN/Documents/Sound recordings/Recording.m4a"))
+
+# directory name of the Recording.m4a is printed. its path printed.
