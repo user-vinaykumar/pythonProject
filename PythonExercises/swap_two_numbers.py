c244e45e536586ef62a00c6dc6aa2c36340d18a8
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-24 16:09:35 +0530

    line formatted.

--- PythonExercises/swap_two_numbers.py
+++ PythonExercises/swap_two_numbers.py
@@ -4,20 +4,19 @@ print(f'num 1 before swapping : {num1}')
 print(f'num 2 before swapping : {num2}')
 print('---------')
 
-num1, num2 = num2, num1   #tuple unpacking method
+num1, num2 = num2, num1  # tuple unpacking method
 
 print(f'num1 after swapping : {num1}')
 print(f'num2 after swapping : {num2}')
 
-
-number1 : int =  20
-number2 : int = 30
+number1: int = 20
+number2: int = 30
 
 print(f'number1 before swapping is : {number1}')
 print(f'number2 before swapping is : {number2}')
 print('---------------')
 
-sum : int = number1 + number2
+sum: int = number1 + number2
 
 number1 = sum - number1
 number2 = sum - number2
