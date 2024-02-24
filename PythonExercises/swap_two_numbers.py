27365367cf745da3295a9ed2922ea1538bde406f
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-24 16:00:38 +0530

    swap two numbers.

---
+++ PythonExercises/swap_two_numbers.py
@@ -0,0 +1,10 @@
+num1 = 19
+num2 = 29
+print(f'num 1 before swapping : {num1}')
+print(f'num 2 before swapping : {num2}')
+print('---------')
+
+num1, num2 = num2, num1   #tuple unpacking method
+
+print(f'num1 after swapping : {num1}')
+print(f'num2 after swapping : {num2}')
