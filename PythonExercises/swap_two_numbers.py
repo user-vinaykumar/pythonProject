a98ce5c81848c34a6aaf12c269f9b0889d15dc3d
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-24 16:33:36 +0530

    approach 3 added for swapping two numbers.

--- PythonExercises/swap_two_numbers.py
+++ PythonExercises/swap_two_numbers.py
@@ -12,6 +12,8 @@ print(f'num2 after swapping : {num2}')
 
 print('------------')
 
+# approach 2
+
 number1: int = 20
 number2: int = 30
 
@@ -26,3 +28,19 @@ number2 = sum - number2
 
 print(f'number1 after swapping is : {number1}')
 print(f'number2 after swapping is : {number2}')
+
+print('--------------')
+
+# approach 3
+
+value1 = input('Enter the value1 : ')
+value2 = input('Enter the value2 : ')
+
+print(f'the value1 before swapping : {value1}')
+print(f'the value2 before swapping : {value2}')
+
+print('----------------')  # by variable1.replace(variable1, variable2) same goes
+# for variable2.   example given down below.
+
+print(f'the value1 after swapping : ', value1.replace(value1, value2))
+print(f'the value2 after swapping : ', value2.replace(value2, value1))
