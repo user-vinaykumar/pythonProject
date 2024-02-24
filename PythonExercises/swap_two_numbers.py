0262ecd34c131bd62c80433645f7dfdeb871699a
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-24 16:08:13 +0530

    swap two numbers by different approach.

--- PythonExercises/swap_two_numbers.py
+++ PythonExercises/swap_two_numbers.py
@@ -8,3 +8,19 @@ num1, num2 = num2, num1   #tuple unpacking method
 
 print(f'num1 after swapping : {num1}')
 print(f'num2 after swapping : {num2}')
+
+
+number1 : int =  20
+number2 : int = 30
+
+print(f'number1 before swapping is : {number1}')
+print(f'number2 before swapping is : {number2}')
+print('---------------')
+
+sum : int = number1 + number2
+
+number1 = sum - number1
+number2 = sum - number2
+
+print(f'number1 after swapping is : {number1}')
+print(f'number2 after swapping is : {number2}')
