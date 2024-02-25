e5f39c90282469cbdbb48eb24a1949761c876cbd
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-25 17:12:25 +0530

    clear the list.

---
+++ PythonExercises/clearList.py
@@ -0,0 +1,20 @@
+# clear a list using any methods.
+
+
+# first approach
+
+lis = [2,3,44,55,12,34,56,78,22,13,41,48,98,67,99]
+k = len(lis)
+
+for i in range(k):
+    lis.pop()
+print(lis)
+
+
+print('-------------')
+# second approach
+
+lis = [22,33,42,55,66,90,7,2,1,3,4,5,6,7]
+
+lis.clear()
+print(lis)
