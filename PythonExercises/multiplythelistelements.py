820206e5a91b0c4b3c3bb65ccdcefec9e88f14cf
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-25 17:40:16 +0530

    multiplying all the elements in a list.

---
+++ PythonExercises/multiplythelistelements.py
@@ -0,0 +1,11 @@
+# multiply all the elements in a list
+
+lis = [1,2,3,4,5,6,7]
+
+count = 1
+
+for i in lis:
+    count*=i
+
+
+print(f'the answer for multiplying all the elements in a {lis} is {count}')
