29e9e811826d933d5b1f1e0d81cb17dfcea1853b
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-25 16:33:51 +0530

    need to solve the programme tomorrow.

---
+++ PythonExercises/fibanocci.py
@@ -0,0 +1,18 @@
+# # check whether the number is fibanocci number or not.
+#
+# number = input('Enter the number : ')
+#
+# k = len(number)
+# b = 0
+# # 01123
+# for j,i in enumerate(number):
+#     if number[(k-1)-b] == number[j-2] + number[j-1]:
+#         fibanocci = True
+#         b+=1
+#     else:
+#         print(f'the given number {number} is not a fibanocci number.')
+#
+# if fibanocci == True:
+#     print(f'the given number {number} is a fibannoci number.')
+#
+#
