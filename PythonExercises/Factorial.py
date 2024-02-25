9ded3c3c823bfdd15e79a4cad5e25a2feec60178
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-25 15:55:17 +0530

    formatted the lines.

--- PythonExercises/Factorial.py
+++ PythonExercises/Factorial.py
@@ -3,9 +3,9 @@
 
 num = int(input('Enter the number : '))
 m = 1
-if num<0:
+if num < 0:
     print(f'the factorial is not there for negative number.')
-elif num==0:
+elif num == 0:
     print(f'the factorial of {num} is : 1')
 else:
     for i in range(1, num + 1):
