1454392ac85e53126637dfacc6512396af9d690a
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-26 13:50:33 +0530

    uncommented the lines.

--- PythonExercises/substring.py
+++ PythonExercises/substring.py
@@ -2,12 +2,12 @@
 
 st = 'welcome to python programming language'
 
-# sub = input('Enter the string : ')
-#
-# if sub in st:
-#     print(f"Yes, the sub string '{sub}' present in the string.")
-# else:
-#     print(f"No, the substring '{sub}' is not present in the string.")
+sub = input('Enter the string : ')
+
+if sub in st:
+    print(f"Yes, the sub string '{sub}' present in the string.")
+else:
+    print(f"No, the substring '{sub}' is not present in the string.")
 
 
 
