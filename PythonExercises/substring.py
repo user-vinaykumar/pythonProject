3b18b579bbe33ea7641006559847a569609d497a
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-26 13:49:50 +0530

    check whether the sub string present in a string -- the second approach.

--- PythonExercises/substring.py
+++ PythonExercises/substring.py
@@ -2,9 +2,29 @@
 
 st = 'welcome to python programming language'
 
-sub = input('Enter the string : ')
+# sub = input('Enter the string : ')
+#
+# if sub in st:
+#     print(f"Yes, the sub string '{sub}' present in the string.")
+# else:
+#     print(f"No, the substring '{sub}' is not present in the string.")
 
-if sub in st:
-    print(f"Yes, the sub string '{sub}' present in the string.")
+
+
+# second approach by using find() function.
+
+
+subs = input('Enter the word : ')
+
+indx = st.find(subs)
+
+if indx>=0:
+    bool = True
 else:
-    print(f"No, the substring '{sub}' is not present in the string.")
+    bool = False
+
+if bool:
+    print(f"Yes, the substring '{subs}' present in the string.")
+else:
+    print(f"No, the substring '{subs}' not present in the string.")
+
