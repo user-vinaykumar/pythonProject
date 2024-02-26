4bcd3828d44f81a8bb0297b17a38dba252bd977b
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-26 18:44:56 +0530

    problem 2 of leet code.

---
+++ W3_practice/problem2.py
@@ -0,0 +1,16 @@
+#Write a Python program to get a list, sorted in increasing order by the last element
+# in each tuple from a given list of non-empty tuples.
+#Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+#Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+
+
+def lastElement(item):
+    return item[-1]
+
+def sort_list(lis):
+    answer = sorted(lis, key=lastElement)
+    print(answer)
+
+
+sort_list([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)])
+
