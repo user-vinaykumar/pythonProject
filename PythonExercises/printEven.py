58d06f33ab851691e48e3ff9f45251bac3ea3ac7
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-26 17:29:24 +0530

    even and odd in the range of numbers.

---
+++ PythonExercises/printEven.py
@@ -0,0 +1,26 @@
+# print even numbers from 1 to 10
+print('even ---------------------')
+lis = []
+for i in range(11):
+    if i%2 == 0:
+        lis.append(i)
+    else:
+        continue
+
+
+print(lis)
+
+print('Odd ------------------------')
+lis2 = []
+for i in range(11):
+    if i%2 != 0:
+        lis2.append(i)
+    else:
+        continue
+
+
+
+print(lis2)
+
+print('---------------------------------')
+
