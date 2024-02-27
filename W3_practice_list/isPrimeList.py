1cc9be871735f03fec41f5aa8c6a42e07d525424
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-27 18:55:22 +0530

    print True if all the elements of the list are prime.

--- W3_practice_list/isPrimeList.py
+++ W3_practice_list/isPrimeList.py
@@ -1,16 +1,16 @@
 # check all the elements in the list are prime numbers.
 # if all the elements are prime numbers then return true or else false.
 
-list1 = [0,3,4,7,9]
-list2 = [3,5,7,13]
-list3 = [1,5,3]
+list1 = [0, 3, 4, 7, 9]
+list2 = [3, 5, 7, 13]
+list3 = [1, 5, 3]
 
 
 def primeResult(numb):
     if numb >= 1:
-        return False # the number is not prime.
+        return False  # the number is not prime.
     else:
-        return True # the number is prime.
+        return True  # the number is prime.
 
 
 def primeCheck(num):
@@ -32,10 +32,11 @@ def isListPrime(item):
     for i in item:
         value = primeResult(primeCheck(i))
         if value == True:
-            total+=1
+            total += 1
         else:
             continue
     return total
+
 
 def result(items):
     if len(items) == isListPrime(items):
@@ -44,9 +45,4 @@ def result(items):
         print(f'the list {items} is : False')
 
 
-
 result(list3)
-
-
-
-
