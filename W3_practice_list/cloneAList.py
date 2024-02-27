96c0222211e72991bbf8b94cc367be7903052edf
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-27 15:12:25 +0530

    clone or copy the list - second approach.

--- W3_practice_list/cloneAList.py
+++ W3_practice_list/cloneAList.py
@@ -2,8 +2,22 @@
 
 brands = ['nike', 'adidas', 'reebok', 'sketchers', 'puma', 'asian']
 
+
+# first approach
+
 def clonealist(item):
     cloned = item
     print(cloned)
 
 clonealist(brands)
+
+
+# second approach
+
+def copyaList(item):
+    copied = item[::-1]
+    copied = copied[::-1]
+
+    print(copied)
+
+copyaList(brands)
