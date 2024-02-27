0c740ac7f25cf025a3537b45d1dc150d38c590c1
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-27 21:17:49 +0530

    no solution even after copying the code from online.

---
+++ W3_practice_list/random.py
@@ -0,0 +1,11 @@
+# # print a random element from the list.
+# import random
+# from random import choice
+#
+#
+# list1 = ['red', 'green', 'grey', 'black', 'purple']
+#
+# def random_element(item):
+#     return choice(item)
+#
+# print(random_element(list1))
