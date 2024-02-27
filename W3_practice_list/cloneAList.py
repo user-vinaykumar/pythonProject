e1d7dbc4e30ef20660081979b40a0c942837cd76
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-27 15:03:59 +0530

    clone or copy the list.

---
+++ W3_practice_list/cloneAList.py
@@ -0,0 +1,9 @@
+# clone a list or copy a list.
+
+brands = ['nike', 'adidas', 'reebok', 'sketchers', 'puma', 'asian']
+
+def clonealist(item):
+    cloned = item
+    print(cloned)
+
+clonealist(brands)
