9c3c40eec549cd0cc41baa1ba90002b059d432ff
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-28 14:06:05 +0530

    complex circular identical problem.

--- W3_practice_list/IdenticalCircle.py
+++ W3_practice_list/IdenticalCircle.py
@@ -4,5 +4,11 @@ list1 = [10, 10, 0, 0, 10]
 list2 = [10, 10, 10, 0, 0]
 list3 = [1, 10, 10, 0, 10]
 
+print('compare list1 and list2')
+print(' '.join(map(str, list1)))
+
+print(' '.join(map(str, list2)) in ' '.join(map(str, list1 * 2)))
+print('compare list1 and list3')
+print(''.join(map(str, list3)) in ''.join(map(str, list1 * 2)))
 
 
