c347d13f80ebe088e54e462541d049c0dedaf357
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-28 14:20:38 +0530

    second smallest number in the list - modified.

--- W3_practice_list/secondSmallest.py
+++ W3_practice_list/secondSmallest.py
@@ -2,12 +2,11 @@
 
 sample = [3,5,6,33,42,7,8,9,89]
 
-def small(item):
-    smallest = min(sample)
+def small(matter):
+    smallest = min(matter)
     return smallest
 
 def solution(item):
-    small(item)
     item.remove(small(item))
     print(f'the second smallest element in the list is : {small(item)}')
 
