248beb30dd67e87369b03afe66beb6492856e52e
user_vk_ <rossilorenzo81773@gmail.com>
2024-02-29 16:57:04 +0530

    converting the list to tuple.

---
+++ W3_tuple/convetList_Tuple.py
@@ -0,0 +1,9 @@
+# convert list to tuple
+
+list = [1,2,3,4,5,6,7,8]
+
+def solution(item):
+    finalResult = tuple(item)
+    print(f'the list {item} is tupled here : {finalResult}')
+
+solution(list)
