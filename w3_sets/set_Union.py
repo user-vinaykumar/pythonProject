5a53e7aef0e2db330b6a90bb39a2228af441068f
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-02 19:07:52 +0530

    set union.

---
+++ w3_sets/set_Union.py
@@ -0,0 +1,9 @@
+# create set union.
+
+sample = {1,2,3,4,5}
+sample2 = {6,7,8}
+
+def setUnion(item, item2):
+    print(f'the union of the sets are : {item.union(item2)}')
+
+setUnion(sample, sample2)
