70dad3eb0707cb89efb3aee1e3d3d8506c4cc97b
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-06 15:38:00 +0530

    basic employee class without self instance.

--- python_Class/class001.py
+++ python_Class/class001.py
@@ -14,12 +14,15 @@ emp_1.first = 'vinay'
 emp_1.last = 'kumar'
 emp_1.email = 'vinay.kumar@company.com'
 emp_1.pay = 50000
+emp_1.location = 'mandya'
+emp_1.experience = 4
 
 emp_2.first = 'sharan'
 emp_2.last = 'gs'
 emp_2.email = 'sharan.gs@gmail.com'
 emp_2.pay = 70000
 
+# print(emp_2.experience)
 print(emp_2.first)
 
 print(emp_1.pay)
