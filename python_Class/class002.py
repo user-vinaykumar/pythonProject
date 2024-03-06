a2eef01a7beecb63eaddfcd5fdf07fc08cd797d9
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-06 15:47:33 +0530

    basic employee class002 without self instance.

---
+++ python_Class/class002.py
@@ -0,0 +1,21 @@
+# creating class with self instance.
+
+class Railways:
+    pass
+
+    # def __init__(self, name, email, ID, phone, location, train):
+    #     self.name = name
+    #     self.email = email
+    #     self.ID = ID
+    #     self.phone = phone
+    #     self.location = location
+    #     self.train = train
+
+# rail1 = Railways('shivalingaiah', 'shiva@gmail.com', 8114006,
+#                  7899097827, 'Mandya', 'Tippu Express')
+# print(rail1)
+print(Railways())
+rail1 = Railways()
+print(rail1)
+rail2 = Railways()
+print(rail2)
