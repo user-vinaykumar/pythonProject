1aa348cc1e6e453a3a2d3c5eed85e5d1449e8413
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-07 18:27:17 +0530

    class initialized with self and methods are called from outside the class with different Instances.

--- python_Class/civilPlan.py
+++ python_Class/civilPlan.py
@@ -21,15 +21,22 @@ class HomePlan:
     def sqr(self, item1, item2):
         return (item1 * item2) / 100
 
+    def sqrList(self, value1, value2, value3, value4):
+        sqrInList = []
+        sqrInList.append(value1)
+        sqrInList.append(value2)
+        sqrInList.append(value3)
+        sqrInList.append(value4)
+        return sqrInList
+
     def totalsqr(self, items):
-        count = 0
-        for i in items:
-            count+=i
-        return count
+        count = sum(items)
+        print(f'the total chadara of the portions are : {count}')
 
     def kitchen(self):
         kitchensqr = HomePlan.sqr(self, self.kitchen_length, self.kitchen_breadth)
         print(f'the sqr of kitchen is : {kitchensqr}')
+
 
 
     def bathroom(self):
@@ -45,14 +52,45 @@ class HomePlan:
         print(f'the sqr of deityroom is : {deityroomsqr}')
 
 
+
+
 vinay = HomePlan(40, 30,
                  9,9,
                  6, 6,
                  5, 3,
                  13, 18)
 
-vinay.kitchen()
-vinay.bathroom()
-vinay.deityroom()
-vinay.livingroom()
+vk = vinay.kitchen()
+vb = vinay.bathroom()
+vd = vinay.deityroom()
+vl = vinay.livingroom()
+
+
+sqr_valuesvinay = vinay.sqrList(vinay.sqr(vinay.kitchen_length, vinay.kitchen_breadth),
+                                vinay.sqr(vinay.bathroom_length, vinay.bathroom_breadth),
+                                vinay.sqr(vinay.deityroom_length, vinay.deityroom_breadth),
+                                vinay.sqr(vinay.livingroom_length, vinay.livingroom_breadth))
+vinay.totalsqr(sqr_valuesvinay)
+print(f'*******************************************')
+
+sharan = HomePlan(30, 40,
+                  10, 10,
+                  5, 5,
+                  13, 13,
+                  3, 5)
+print('---------------------')
+
+sk = sharan.kitchen()
+sb = sharan.bathroom()
+sl = sharan.livingroom()
+sd = sharan.deityroom()
+
+sqr_valuessharan = sharan.sqrList(sharan.sqr(sharan.kitchen_length, sharan.kitchen_breadth),
+                                  sharan.sqr(sharan.bathroom_length, sharan.bathroom_breadth),
+                                  sharan.sqr(sharan.deityroom_length, sharan.deityroom_breadth),
+                                  sharan.sqr(sharan.livingroom_length, sharan.livingroom_breadth))
+
+
+sharan.totalsqr(sqr_valuessharan)
 # vinay.sqr(34, 43)
+
