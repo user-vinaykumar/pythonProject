ff61a77539c0ee2456e937603492b5b0c631fbc0
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-13 19:00:05 +0530

    class static methods are expalined here.

---
+++ python_Class/staticMethod001.py
@@ -0,0 +1,22 @@
+# static method example is shown here in this.
+
+class Employee:
+
+    def percentage(self):  # instance methods will take self as their first argument.
+        pass
+
+    @classmethod
+    def network(cls):  # class methods will take cls as their first argument.
+        pass
+
+    @staticmethod
+    def is_weekdaY(day):  # static methods neither take self nor cls as their arguments but receive what is given.
+        if day.weekday() == 5:
+            return False
+        elif day.weekday() == 6:
+            return False
+        return True
+
+import datetime
+my_date = datetime.datetime(2023, 3, 13)
+print(Employee.is_weekdaY(my_date))
