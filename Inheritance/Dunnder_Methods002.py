694fab0f8879f894cc2e8f68e18c6fbc2feb2218
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-22 16:56:02 +0530

    class with constructor is created.

---
+++ Inheritance/Dunnder_Methods002.py
@@ -0,0 +1,10 @@
+# dunnder methods part 2
+
+class Employee:
+
+    def __init__(self, firstname, lastname, pay, company):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.pay = pay
+        self.company = company
+
