568fc82f4d616a5811d1422d23acf5f5862c9d67
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-22 16:59:16 +0530

    Method fullname() is created.

--- Inheritance/Dunnder_Methods002.py
+++ Inheritance/Dunnder_Methods002.py
@@ -2,9 +2,14 @@
 
 class Employee:
 
+    raise_amount = 1.10
+
     def __init__(self, firstname, lastname, pay, company):
         self.firstname = firstname
         self.lastname = lastname
         self.pay = pay
         self.company = company
+
+    def fullname(self):
+        return f'full name : {self.firstname} {self.lastname}'
 
