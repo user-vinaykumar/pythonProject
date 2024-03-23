6d90bc421d33ae337a9db86aedb0b65ceb5cce70
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-23 16:20:40 +0530

    Employee class is created with constructor.

---
+++ Inheritance/getter_setter.py
@@ -0,0 +1,10 @@
+# inheritance concepts with getter and setters.
+
+class Employee:
+
+    def __init__(self, firstname, lastname, pay, company):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.pay = pay
+        self.company = company
+
