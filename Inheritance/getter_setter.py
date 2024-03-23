b6e47d9eec2d98fe827b2e6bac71ae3f637ba924
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-23 16:28:31 +0530

    getters and setters of the python oops.

--- Inheritance/getter_setter.py
+++ Inheritance/getter_setter.py
@@ -7,4 +7,24 @@ class Employee:
         self.lastname = lastname
         self.pay = pay
         self.company = company
+    @property
+    def email(self):
+        return f'{self.firstname}.{self.lastname}@{self.company}.com'
 
+    @property
+    def fullname(self):
+        return f'{self.firstname} {self.lastname}'
+
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.firstname = first
+        self.lastname = last
+
+emp1 = Employee('vinay', 'kumar', 89999, 'virtusa')
+print(emp1.fullname)
+emp1.fullname = 'sharan gs'
+print(emp1.fullname)
+print(emp1.email)
+emp1.fullname = 'vinay kumar'
+print(emp1.email)
