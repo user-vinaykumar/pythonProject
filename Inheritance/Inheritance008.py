feb18573ecf9842a42cc8ae41c2f2388cfd0d81f
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-23 15:15:53 +0530

    Developer class is created and it Inherits the properties of Employee class.

--- Inheritance/Inheritance008.py
+++ Inheritance/Inheritance008.py
@@ -18,3 +18,10 @@ class Employee:
     def email(self):
         return (f'email of the employee : {self.firstname}.{self.lastname}'
                 f'@{self.company}.com')
+
+class Developer(Employee):
+
+    def __init__(self, firstname, lastname, pay, company, programming_language):
+        super().__init__(firstname, lastname, pay, company)
+        self.programming_language = programming_language
+
