027c4a53ac17588bdb127c4d9999de9245605443
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-24 14:01:57 +0530

    __add__ method is added to Employee class.

--- Inheritance/Inheritance_concepts.py
+++ Inheritance/Inheritance_concepts.py
@@ -34,6 +34,9 @@ class Employee:
     def __str__(self):
         return f'email of the employee : {self.email()}'
 
+    def __add__(self, other):
+        return self.pay + other.pay
+
 
 class Developer(Employee):
 
