d1446b46b592e35fe60abcb042e650a4f396d57f
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-25 15:53:23 +0530

    employee class is created and constructor is defined.

---
+++ Inheritance/Coreyschafer_OOPS.py
@@ -0,0 +1,10 @@
+# corey schafer python oops.
+
+class Employee:
+
+    def __init__(self, firstname, lastname, pay, company):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.pay = pay
+        self.company = company
+
