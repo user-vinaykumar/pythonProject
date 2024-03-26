176a6356c0f9580142274068032efd66c8def2b0
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-26 16:27:43 +0530

    class method emp_instance is created.

--- Inheritance/Inheritancebycoreyschafer.py
+++ Inheritance/Inheritancebycoreyschafer.py
@@ -12,3 +12,8 @@ class Employee:
     def set_raise_amount(cls, amount):
         cls.raise_amount = amount
 
+    @classmethod
+    def emp_instance(cls, item):
+        first, last, pay, company = item.split(' ')
+        return cls(first, last, pay, company)
+
