4414c7fdcf21a738eff75e17e03925ee094796dc
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-28 14:52:51 +0530

    remove_emp method is created in Manager class.

--- Dictionary/Inheritance.py
+++ Dictionary/Inheritance.py
@@ -66,3 +66,9 @@ class Manager(Employee):
         else:
             pass
 
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            list.remove(emp)
+        else:
+            pass
+
