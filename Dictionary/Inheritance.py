048d8dba4f6733e7ed2e9470e086e0484aa6b601
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-28 14:51:47 +0530

    add_emp method is created in Manager class.

--- Dictionary/Inheritance.py
+++ Dictionary/Inheritance.py
@@ -60,3 +60,9 @@ class Manager(Employee):
         else:
             self.employees = employees
 
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            list.append(emp)
+        else:
+            pass
+
