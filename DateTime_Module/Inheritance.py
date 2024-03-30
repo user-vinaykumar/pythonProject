907a7e9b43b89fdef18e1dbc9203db85b54eb6bf
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-30 15:03:11 +0530

    Manager class created with constructor by inheriting Employee class.

--- DateTime_Module/Inheritance.py
+++ DateTime_Module/Inheritance.py
@@ -49,3 +49,12 @@ class Developer(Employee):
         super().__init__(firstname, lastname, pay, company)
         self.proglang = proglang
 
+class Manager(Employee):
+
+    def __init__(self, firstname, lastname, pay, company, employees=None):
+        super().__init__(firstname, lastname, pay, company)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
