929053c888644b18a18b825800c0c744b183f3e1
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-30 15:05:44 +0530

    remove_emp method is created inside the Employee class.

--- DateTime_Module/Inheritance.py
+++ DateTime_Module/Inheritance.py
@@ -64,4 +64,10 @@ class Manager(Employee):
         else:
             pass
 
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            list.remove(emp)
+        else:
+            pass
+
 
