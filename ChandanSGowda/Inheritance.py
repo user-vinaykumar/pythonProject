51f3bb1605959c0d3b30ea38c49365e8745b718c
user_vk_ <rossilorenzo81773@gmail.com>
2024-03-30 15:23:59 +0530

    Inheritance concepts is learned as shown in corey schafer yt learning next chapter from tomorrow.

---
+++ ChandanSGowda/Inheritance.py
@@ -0,0 +1,75 @@
+class Employee:
+
+    def __init__(self, firstname, lastname, pay, company):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.pay = pay
+        self.company = company
+
+    raise_amount = 2.20
+
+    @property
+    def fullname(self):
+        return f'{self.firstname} {self.lastname}'
+
+    @property
+    def email(self):
+        return f'{self.firstname}.{self.lastname}@{self.company}.com'
+
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.firstname = first
+        self.lastname = last
+
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def emp_instance(cls, item):
+        first, last, pay, company = item.split(' ')
+        return cls(first, last, pay, company)
+
+    def apply_raise(self):
+        self.pay = int(float(self.pay) * float(self.raise_amount))
+
+    def __repr__(self):
+        return f'{self.fullname}'
+
+    def __str__(self):
+        return f'{self.email}'
+
+    def __add__(self, other):
+        return self.pay + other.pay
+
+class Developer(Employee):
+
+    def __init__(self, firstname, lastname, pay, company, proglang):
+        super().__init__(firstname, lastname, pay, company)
+        self.proglang = proglang
+
+class Manager(Employee):
+
+    def __init__(self, firstname, lastname, pay, company, employees=None):
+        super().__init__(firstname, lastname, pay, company)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+        else:
+            pass
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+        else:
+            pass
+
+    def print(self):
+        for emp in self.employees:
+            print(f'--> {emp.fullname}')
