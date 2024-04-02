21ab340b18e49b8a0995c070de2bc564e3beeeeb
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-02 13:53:25 +0530

    Employee class created along with the constructor.

---
+++ OS_Module/inheritance005.py
@@ -0,0 +1,10 @@
+class Employee:
+
+    def __init__(self, firstname, lastname, pay, company):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.pay = pay
+        self.company = company
+
+
+
