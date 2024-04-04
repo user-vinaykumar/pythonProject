4c1c7124c840686a3dbdc16af7c1fe424aa04c2f
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-04 11:19:19 +0530

    Employee class created with constructor inside in it.

---
+++ Inheritance/oops002.py
@@ -0,0 +1,7 @@
+class Employee:
+
+    def __init__(self, firstname, lastname, pay, company):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.pay = pay
+        self.company = company
