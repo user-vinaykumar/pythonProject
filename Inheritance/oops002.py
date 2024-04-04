60c09913772ec3d4da5ae088191309a277dcbe25
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-04 11:20:34 +0530

    fullname method is created inside the Employee class.

--- Inheritance/oops002.py
+++ Inheritance/oops002.py
@@ -5,3 +5,7 @@ class Employee:
         self.lastname = lastname
         self.pay = pay
         self.company = company
+
+    def fullname(self):
+        return f'{self.firstname} {self.lastname}'
+
