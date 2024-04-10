4134b13dc71d4a9c852f4f1432d6ec5678d62544
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-10 09:11:07 +0530

    fullname getter feature is created.

--- ChandanSGowda/oopspart222.py
+++ ChandanSGowda/oopspart222.py
@@ -20,6 +20,7 @@ class Employee:
     def apply_raise(self):
         self.pay = int(float(self.pay) * float(self.raise_percent))
 
+    @property
     def fullname(self):
         return f'{self.firstname} {self.lastname}'
 
