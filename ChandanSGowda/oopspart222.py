b5fc4409d45037395c6c492be9d29be12d2eeed0
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-10 09:05:47 +0530

    set_raise_percent method is created.

--- ChandanSGowda/oopspart222.py
+++ ChandanSGowda/oopspart222.py
@@ -6,4 +6,9 @@ class Employee:
         self.pay = pay
         self.company = company
 
-    raise_amount = 1.10
+    raise_percent = 1.10
+
+    @classmethod
+    def set_raise_percent(cls, percent):
+        cls.raise_percent = percent
+
