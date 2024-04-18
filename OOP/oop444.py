25ca7c38e76c25f4235a45146d44bbba93bbf813
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-18 14:18:24 +0530

    fullname method is created inside the Employee class.

--- OOP/oop444.py
+++ OOP/oop444.py
@@ -12,3 +12,10 @@ class Employee:
     def set_raise_percent(cls, percent):
         cls.raise_percent = percent
 
+    @classmethod
+    def emp_instance(cls, item):
+        first, last, pay, comp = item.split(' ')
+        return cls(first, last, pay, comp)
+
+    def fullname(self):
+        return f'{self.firstname} {self.lastname}'
