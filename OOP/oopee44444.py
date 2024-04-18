23014ab9eebaa066becd0d39b22b3a89ea6f8d7e
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-18 17:10:23 +0530

    Employee class is created and constructor is created along with it.

---
+++ OOP/oopee44444.py
@@ -0,0 +1,9 @@
+class Employee:
+
+    def __init__(self, firstname, lastname, pay, company):
+        self.firstname = firstname
+        self.lastnam = lastname
+        self.pay = pay
+        self.company = company
+
+
