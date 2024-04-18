f5c1fb8e6a95fa087632a28fbefaddeea93c5830
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-18 17:38:46 +0530

    dunnder methods are created inside the Employee class.

--- OOP/oopee44444.py
+++ OOP/oopee44444.py
@@ -35,3 +35,11 @@ class Employee:
         first, last, pay, comp = item.split(' ')
         return cls(first, last, pay, comp)
 
+    def __repr__(self):
+        return (f'{self.fullname}')
+
+    def __add__(self, other):
+        return self.pay + other.pay
+
+    def __str__(self):
+        return self.email
