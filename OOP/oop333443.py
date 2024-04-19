c33ba52d7108abd1bc18859148dcefedfa0856c2
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-19 13:17:49 +0530

    fullname setter method is created inside the Employee class.

--- OOP/oop333443.py
+++ OOP/oop333443.py
@@ -13,3 +13,9 @@ class Employee:
     def email(self):
         return f'{self.firstname}.{self.lastname}@{self.company}.com'
 
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.firstname = first
+        self.lastname = last
+
