621790cb1910a8b92aef23e77dc83acc24468a41
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-24 16:42:08 +0530

    Manager class is created and Inherited the properties from the Employee class.

--- OOP/object.py
+++ OOP/object.py
@@ -49,3 +49,13 @@ class Developer(Employee):
     def __init__(self, firstname, lastname, pay, company, proglang):
         super().__init__(firstname, lastname, pay, company)
         self.proglang = proglang
+
+class Manager(Employee):
+
+    def __init__(self, firstname, lastname, pay, company, employeelist=None):
+        super().__init__(firstname, lastname, pay, company)
+        if employeelist is None:
+            self.employeelist = []
+        else:
+            self.employeelist = employeelist
+
