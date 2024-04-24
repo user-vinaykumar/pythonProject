6c10353c301831cbde2cabaf4541655da9fd2693
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-24 16:54:22 +0530

    Employee class is created.

---
+++ OOP/oopsconcepts.py
@@ -0,0 +1,8 @@
+class Employee:
+
+    def __init__(self, firstname, lastname, pay, company):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.pay = pay
+        self.compnay = company
+
