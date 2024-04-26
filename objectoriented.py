2681cfde9053263fbf2ce67cf3124224a47a942a
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-26 15:17:58 +0530

    email method is created inside the Employee class.

--- objectoriented.py
+++ objectoriented.py
@@ -9,3 +9,6 @@ class Employee:
     def fullname(self):
         return f'{self.firstname} {self.lastname}'
 
+    def email(self):
+        return f'{self.firstname}.{self.lastname}@{self.company}.com'
+
