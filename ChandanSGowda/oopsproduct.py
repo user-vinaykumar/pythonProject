ab64673711e109c8da96ad23b19143327d006e3f
user_vk_ <rossilorenzo81773@gmail.com>
2024-04-30 15:03:52 +0530

    add_emp method is created inside the Manager class.

--- ChandanSGowda/oopsproduct.py
+++ ChandanSGowda/oopsproduct.py
@@ -49,5 +49,20 @@ class Developer(Employee):
         super().__init__(firstname, lastname, pay, company)
         self.proglang = proglang
 
+class Manager(Employee):
+
+    def __init__(self, firstname, lastname, pay, company, employeelist=None):
+        super().__init__(firstname, lastname, pay, company)
+        if employeelist is None:
+            self.employeelist = []
+        else:
+            self.employeelist = employeelist
+
+    def add_emp(self, emp):
+        if emp not in self.employeelist:
+            self.employeelist.append(emp)
+        else:
+            pass
+
 
 
