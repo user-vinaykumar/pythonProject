25dbdd55cdb46739a32742d8369f9bf256589690
user_vk_ <rossilorenzo81773@gmail.com>
2024-05-04 12:45:33 +0530

    add_emp method is created inside the Manager class.

--- DateTime_Module/objectoriented.py
+++ DateTime_Module/objectoriented.py
@@ -59,3 +59,9 @@ class Manager(Employee):
         else:
             self.employeelist = employeelist
 
+    def add_emp(self, emp):
+        if emp not in self.employeelist:
+            self.employeelist.append(emp)
+        else:
+            pass
+
