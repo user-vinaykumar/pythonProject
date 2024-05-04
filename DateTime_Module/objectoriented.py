7c4d2038346b558e2b88ab2ea05e971591174593
user_vk_ <rossilorenzo81773@gmail.com>
2024-05-04 12:41:26 +0530

    Developer class is inherited by Employee class.

--- DateTime_Module/objectoriented.py
+++ DateTime_Module/objectoriented.py
@@ -43,3 +43,10 @@ class Employee:
     def __add__(self, other):
         return self.pay + other.pay
 
+
+class Developer(Employee):
+
+    def __init__(self, firstname, lastname, pay, company, proglang):
+        super().__init__(firstname, lastname, pay, company)
+        self.proglang = proglang
+
