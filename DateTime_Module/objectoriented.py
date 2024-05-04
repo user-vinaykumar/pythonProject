39d4a831e64be3f7700487fedbf41316cc3df8bc
user_vk_ <rossilorenzo81773@gmail.com>
2024-05-04 12:38:59 +0530

    emp_instance method is created so that we can create any instance of the Employee class now without any instance creation.

--- DateTime_Module/objectoriented.py
+++ DateTime_Module/objectoriented.py
@@ -29,3 +29,8 @@ class Employee:
     def apply_raise(self):
         self.pay = int(float(self.pay) * float(self.raise_amount))
 
+    @classmethod
+    def emp_instance(cls, item):
+        first, last, pay, comp = item.split(' ')
+        return cls(first, last, pay, comp)
+
