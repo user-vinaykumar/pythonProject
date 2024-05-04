7df7101d2c4033d39b36992ec2b886dd0122c815
user_vk_ <rossilorenzo81773@gmail.com>
2024-05-04 12:32:39 +0530

    email method is created.

--- DateTime_Module/objectoriented.py
+++ DateTime_Module/objectoriented.py
@@ -9,3 +9,5 @@ class Employee:
     def fullname(self):
         return f'{self.firstname} {self.lastname}'
 
+    def email(self):
+        return f'{self.firstname}.{self.lastname}@{self.company}.com'
