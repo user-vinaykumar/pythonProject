c83e925756a4214a678b455f966e657326bc9307
user_vk_ <rossilorenzo81773@gmail.com>
2024-05-04 12:46:40 +0530

    print_emp method is created inside the Manager class.

--- DateTime_Module/objectoriented.py
+++ DateTime_Module/objectoriented.py
@@ -65,9 +65,3 @@ class Manager(Employee):
         else:
             pass
 
-    def remove_emp(self, emp):
-        if emp in self.employeelist:
-            self.employeelist.remove(emp)
-        else:
-            pass
-
