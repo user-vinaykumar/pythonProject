fc46fb8c05d1ddded97441d307aa3f1379ba30f8
user_vk_ <rossilorenzo81773@gmail.com>
2024-05-05 11:09:27 +0530

    employee class is created and constructor is also created inside the class.

---
+++ ChandanSGowda/object002.py
@@ -0,0 +1,9 @@
+class Employee:
+
+    def __init__(self, firstname, lastname, pay, company):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.pay = pay
+        self.company = company
+
+
