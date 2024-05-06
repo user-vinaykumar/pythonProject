4723b3576a2180c030132c62b60cc7adfa0b7c2b
user_vk_ <rossilorenzo81773@gmail.com>
2024-05-06 11:45:01 +0530

    Royal Challengers Bengaluru class is created and its constructor is being created.

---
+++ ChandanSGowda/rcb.py
@@ -0,0 +1,10 @@
+class RoyalChallengersBengaluru():
+
+    def __init__(self, firstname, lastname, country, package, handed):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.country = country
+        self.package = package
+        self.handed = handed
+
+
