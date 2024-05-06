b51088aaeb282fc7739da05c74c74296a01c1f8b
user_vk_ <rossilorenzo81773@gmail.com>
2024-05-06 11:48:48 +0530

    player's email is created within Players class.

--- ChandanSGowda/rcb.py
+++ ChandanSGowda/rcb.py
@@ -1,13 +1,19 @@
-class RoyalChallengersBengaluru():
+class Players():
 
-    def __init__(self, firstname, lastname, country, package, handed):
+    def __init__(self, firstname, lastname, country, package, handed, team):
         self.firstname = firstname
         self.lastname = lastname
         self.country = country
         self.package = package
         self.handed = handed
+        self.team = team
+
 
     @property
     def fullname(self):
         return f'{self.firstname} {self.lastname}'
+
+
+    def email(self):
+        return f'{self.firstname}.{self.lastname}@{self.team}.com'
 
