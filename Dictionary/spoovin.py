03f645e6ca5f8721f1746c1589f0053d4ace1afd
user_vk_ <rossilorenzo81773@gmail.com>
2024-05-08 16:13:41 +0530

    email method is created inside the spoovin.py file.

--- Dictionary/spoovin.py
+++ Dictionary/spoovin.py
@@ -10,3 +10,7 @@ class Employee:
     def fullname(self):
         return f'{self.firstname} {self.lastname}'
 
+
+    def email(self):
+        return f'{self.firstname}.{self.lastname}@{self.company}.com'
+
